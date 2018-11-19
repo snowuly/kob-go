29d4b77edba62525f674f56095541931b21a1c4e
chentao.ct <chentao.ct@alibaba-inc.com>
2018-11-19 17:41:08 +0800

    doc

--- README.md
+++ README.md
@@ -5,12 +5,21 @@
 ```go
 var app kob.App
 
-app.Get("/name/:name", func(ctx context.Context, w http.ResponseWriter, r *http.Request, next kob.NextFunc) {
+app.Get(
+	"/name/:name",
+	func(ctx context.Context, w http.ResponseWriter, r *http.Request, next kob.NextFunc) {
+		start := time.Now()
+		next(ctx)
+		w.Write([]byte(fmt.Sprintf("\nprocess cost: %d ns", time.Since(start))))
+	},
+	func(ctx context.Context, w http.ResponseWriter, r *http.Request, next kob.NextFunc) {
+		params := kob.GetParams(ctx)
+		w.Write([]byte("hello "))
+		w.Write([]byte(params["name"]))
+	},
+)
 
-	params := kob.GetParams(ctx)
-	w.Write([]byte("hello "))
-	w.Write([]byte(params["name"]))
-})
+	app.Listen(":8080")
 
 app.Listen(":8080")
 ```
--- example.go
+++ example.go
@@ -4,20 +4,29 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"kob"
 	"net/http"
+	"time"
 )
 
 func main() {
 
 	var app kob.App
 
-	app.Get("/name/:name", func(ctx context.Context, w http.ResponseWriter, r *http.Request, next kob.NextFunc) {
-
-		params := kob.GetParams(ctx)
-		w.Write([]byte("hello "))
-		w.Write([]byte(params["name"]))
-	})
+	app.Get(
+		"/name/:name",
+		func(ctx context.Context, w http.ResponseWriter, r *http.Request, next kob.NextFunc) {
+			start := time.Now()
+			next(ctx)
+			w.Write([]byte(fmt.Sprintf("\nprocess cost: %d ns", time.Since(start))))
+		},
+		func(ctx context.Context, w http.ResponseWriter, r *http.Request, next kob.NextFunc) {
+			params := kob.GetParams(ctx)
+			w.Write([]byte("hello "))
+			w.Write([]byte(params["name"]))
+		},
+	)
 
 	app.Listen(":8080")
 }
