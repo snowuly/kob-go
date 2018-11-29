3b98381a207af351619bd40a6070af6287787f66
chentao.ct <chentao.ct@alibaba-inc.com>
2018-11-29 15:10:39 +0800

    wip

--- app.go
+++ app.go
@@ -58,11 +58,11 @@ func (app *App) Post(path string, fns ...HandlerFunc) {
 	app.Route("POST", path, fns...)
 }
 
-func (app *App) Listen(addr string) {
-	http.ListenAndServe(addr, app)
+func (app *App) Listen(addr string) error {
+	return http.ListenAndServe(addr, app)
 }
-func (app *App) ListenTLS(addr, cert, key string) {
-	http.ListenAndServeTLS(addr, cert, key, app)
+func (app *App) ListenTLS(addr, cert, key string) error {
+	return http.ListenAndServeTLS(addr, cert, key, app)
 }
 
 func GetParams(ctx context.Context) map[string]string {
